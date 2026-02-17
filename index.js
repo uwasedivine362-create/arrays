25ea9e6bc96b34c346d30e0bc0d96b2d2637f5eb
uwasedivine362@gmail.com <uwasedivine362@gmail.com>
2026-02-17 15:26:15 +0200

    fist exercises

---
+++ index.js
@@ -0,0 +1,33 @@
+//exercise1 :array initialization and property Access
+const productCategories =["book" ,"Electronics" ,"Clothing" ,"Home & kitchen", "toys & Game"];
+console.log(productCategories[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//EXERCISES 2:push ,pop ,shift ,unshift,shift, splice
+let inventory=
+inventory.splice(1, 1, 25,35);
+console.log(inventory);
+
+
+
+
+
+
+// EXERCISE3 :shallow copying of arrays and the reference trap
+//a
+const originalscore= []
